54a2fef7a0923597c29317e82e88686757f3f257
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-08-18 15:53:04 +0200

    Add ESLint configuration file to enforce coding standards and integrate with Prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,13 +4,23 @@ import tseslint from "typescript-eslint"
 import pluginReact from "eslint-plugin-react"
 import prettier from "eslint-plugin-prettier"
 import eslintConfigPrettier from "eslint-config-prettier/flat"
+import { fileURLToPath } from "url"
+import { dirname } from "path"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default tseslint.config([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     plugins: { js, prettier },
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json", "./packages/*/tsconfig.json", "./apps/*/tsconfig.json"],
+      },
+    },
     rules: {
       "prettier/prettier": "warn",
       "@typescript-eslint/consistent-type-imports": "error",
