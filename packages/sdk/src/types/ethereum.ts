0a8b6716a30fdde48181f472e97e263b862e5c15
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-08-19 13:14:33 +0200

    refactor(sdk): remove old domain and types structure in favor of modular packages

--- packages/sdk/src/types/ethereum.ts
+++
@@ -1,3 +0,0 @@
-import type { Abi, Address, PublicClient, Transport, SignTypedDataParameters, Chain, Signature } from "viem"
-
-export type { Abi, Address, PublicClient, Transport, SignTypedDataParameters, Chain, Signature }
